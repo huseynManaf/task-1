ebf0ab800eec6d5428c6a9aa595129833ba1831b
huseynManaf <huseynim@code.edu.az>
2022-12-29 03:14:38 -0800

    init

--- task1/Program.cs
+++ task1/Program.cs
@@ -6,20 +6,49 @@
         {
 
             #region task1
-            int number = int.Parse(Console.ReadLine());
-            int countDigit = 0;
-            while (number > 0) ;
-            {
-                number /= 10;
-                countDigit++;
-            }
-            Console.WriteLine(countDigit);
+            //int number = int.Parse(Console.ReadLine());
+
+            //int countDigit = 0;
+
+            //while (number > 0)
+            //{
+            //    number /= 10;
+            //    countDigit++;
+            //}
+            //Console.WriteLine(countDigit);
+
+            //int countDigitLog = (int)Math.Log10(number) + 1;
+            //int r, i;
+
+            //if (countDigitLog % 2 == 0)
+            //    i = 0;
+            //else
+            //    i = 1;
+
+            //i = countDigitLog % 2 == 0 ? 0 : 1;
+
+            //i = countDigitLog;
+
+            //while (number != 0)
+            //{
+            //    r = number % 10;
+            //    number /= 10;
+
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(r);
+            //    }
+
+            //    i--;
+            //}
+            //Console.WriteLine(countDigitLog);
+
             #endregion end of task1
             #region task2
-
-            //int count = (int)Math.Log10(number) + 1;
+            //int number = int.Parse(Console.ReadLine());
             //int r;
             //int sum = 0;
+
             //while (number != 0)
             //{
             //    r = number % 10;
@@ -27,38 +56,23 @@
             //    sum += r * count;
             //    count--;
             //}
-            //int a = int.Parse(Console.ReadLine());
-            //int b = 7;
-            //Math.Pow(a, b);
+
             //Console.WriteLine(sum);
             #endregion end of task2
             #region task3
             //int number = int.Parse(Console.ReadLine());
-            int count = (int)Math.Log10(number) + 1;
-            int r;
-            int sum = 0;
-            while (number != 0)
-            {
-                r = number % 10;
-                number /= 10;
-                sum += r * count;
-                count--;
-            }
-            int a = int.Parse(Console.ReadLine());
-            int b = 7;
-            Math.Pow(a, b);
-            Console.WriteLine(sum);
-            int number = int.Parse(Console.ReadLine());
-            int count = (int)Math.Log10(number) + 1;
-            if (count == 5)
-            {
-                int result = (400000 + number) * 10 + 2;
-                Console.WriteLine(result);
-            }
-            else
-            {
-                Console.WriteLine("Eded 5 reqemli olmalidir");
-            }
+            //12345 400000 + 12345 = 412345 * 10 + 2;
+            //int count = (int)Math.Log10(number) + 1;
+
+            //if (count == 5)
+            //{
+            //    int result = (400000 + number) * 10 + 2;
+            //    Console.WriteLine(result);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Eded 5 reqemli olmalidir");
+            //}
             #endregion end of task3
             #region task4
 
